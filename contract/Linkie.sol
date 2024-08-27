3ec3c1e00e43d5229f0a39071cf5d96181a008f9
iamendy <17215830+iamendy@users.noreply.github.com>
2024-08-27 16:52:08 +0100

    update: rollback streaming

--- contract/Linkie.sol
+++ contract/Linkie.sol
@@ -1,141 +1,152 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { ISuperfluid, ISuperToken } from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
-import { SuperTokenV1Library } from "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
+import "interfaces/IERC20.sol";
 
-//TODOS: add events
 contract Linkie {
-  using SuperTokenV1Library for ISuperToken;
+  address public USDT;
 
-  uint256 systemFee;
-  address owner;
-  ISuperToken public usdtx; // SuperToken to be streamed
-
-  struct Campaign {
-    string id;
-    address creator;
-    address creative;
-    uint256 milestoneCount;
-    uint96 paymentCount;
-    uint256 amount;
-    int96 baseFlowRate;
-    int96 currentFlowRate;
-    bool isStreaming;
-    bool isCompleted;
+  enum Status {
+    PENDING,
+    PAID,
+    COMPLETED,
+    CANCELLED
   }
 
-  struct Creative {
-    uint256 xp;
+  struct Listing {
+    bytes32 id;
+    address seller;
+    address buyer;
+    uint256 rate;
+    uint quantity;
+    Status status;
   }
 
-  mapping(string => Campaign) campaigns;
-  mapping(address => Creative) public creatives;
+  mapping(bytes32 => mapping(address => Listing)) public listings;
 
-  constructor(uint256 _systemFee, ISuperToken _usdtx) {
-    systemFee = _systemFee;
-    owner = msg.sender;
-    usdtx = _usdtx; // Initialize the SuperToken
+  // Additional data structures to keep track of id keys
+  bytes32[] listingKeys;
+  mapping(address => bytes32[]) addressToListing;
+
+  event newListing(
+    bytes32 indexed id,
+    address indexed seller,
+    uint256 rate,
+    uint256 quantity
+  );
+
+  event ListingPaid(
+    bytes32 indexed id,
+    address indexed seller,
+    address indexed buyer,
+    uint amount,
+    uint quantity
+  );
+
+  constructor(address _USDT) {
+    USDT = _USDT;
   }
 
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Only owner allowed");
-    _;
-  }
-
-  //this activates the campaign
-  function createCampaign(
-    string calldata _campaignId,
-    uint256 _milestoneCount,
-    uint256 _amount,
-    int96 _baseFlowRate
-  ) external payable {
-    require(_amount > 0, "invalid amount");
-    //saves the campaign on-chain
-    campaigns[_campaignId] = Campaign({
-      id: _campaignId,
-      creator: msg.sender,
-      creative: address(0),
-      amount: msg.value,
-      milestoneCount: _milestoneCount,
-      paymentCount: 0,
-      baseFlowRate: _baseFlowRate,
-      currentFlowRate: _baseFlowRate,
-      isStreaming: false,
-      isCompleted: false
+  function addListing(bytes32 _id, uint _rate, uint _quantity) public {
+    // Store listing to user
+    listings[_id][msg.sender] = Listing({
+      id: _id,
+      seller: msg.sender,
+      buyer: address(0),
+      rate: _rate,
+      quantity: _quantity,
+      status: Status.PENDING
     });
+
+    // If this is a new key, add it to the listingKeys array
+    bool isNewBytesKey = true;
+    for (uint256 i = 0; i < listingKeys.length; i++) {
+      if (listingKeys[i] == _id) {
+        isNewBytesKey = false;
+        break;
+      }
+    }
+    if (isNewBytesKey) {
+      listingKeys.push(_id);
+    }
+
+    // If this address has not been associated with this key, add the key
+    bool isNewAddressKey = true;
+    bytes32[] storage bytesKeys = addressToListing[msg.sender];
+    for (uint256 i = 0; i < bytesKeys.length; i++) {
+      if (bytesKeys[i] == _id) {
+        isNewAddressKey = false;
+        break;
+      }
+    }
+    if (isNewAddressKey) {
+      addressToListing[msg.sender].push(_id);
+    }
   }
 
-  //matches a creative to a campaign
-  function matchCampaign(
-    string calldata campaignId,
-    address _creative
+  function getListing(
+    bytes32 _id,
+    address _seller
+  ) public view returns (Listing memory) {
+    return listings[_id][_seller];
+  }
+
+  function getAllListingsForAddress(
+    address _seller
+  ) public view returns (Listing[] memory) {
+    bytes32[] storage bytesKeys = addressToListing[_seller];
+    Listing[] memory structs = new Listing[](bytesKeys.length);
+
+    for (uint256 i = 0; i < bytesKeys.length; i++) {
+      structs[i] = listings[bytesKeys[i]][_seller];
+    }
+
+    return structs;
+  }
+
+  function payForListing(
+    bytes32 _id,
+    address _seller,
+    uint _quantity,
+    uint _amount
   ) external {
-    require(campaigns[campaignId].creator == msg.sender, "Not permitted");
-    campaigns[campaignId].creative = _creative;
-  }
+    //retrieve the listing
+    Listing storage listing = listings[_id][_seller];
 
-  //pays each milestone on creator/business approval  with Superfluid streaming
-  function payMilestone(string calldata campaignId) external {
-    Campaign storage campaign = campaigns[campaignId];
+    //calculate price
+    uint price = listing.rate * _quantity;
 
-    require(campaign.creator == msg.sender, "Not permitted");
     require(
-      campaign.paymentCount < campaign.milestoneCount,
-      "Payment already completed"
+      listing.status == Status.PENDING || listing.status == Status.PAID,
+      "Invalid listing"
     );
+    require(_quantity <= listing.quantity, "Invalid quantity");
+    require(_amount >= price, "Invalid amount");
 
-    // If this is the first milestone, start the stream
-    if (!campaign.isStreaming) {
-      usdtx.createFlow(campaign.creative, campaign.baseFlowRate);
-      campaign.isStreaming = true;
+    // calculate charge -
+    // note: Linkie only charges on rate, not on quantity for sellers cheaper experience 😊
+    uint charge = deductCharge(listing.rate);
+
+    //transfer balance to seller after charge
+    IERC20(USDT).transferFrom(msg.sender, listing.seller, price - charge);
+    //deduct charge
+    IERC20(USDT).transferFrom(msg.sender, address(this), charge);
+
+    listing.buyer = msg.sender;
+    listing.quantity -= _quantity;
+
+    if (listing.quantity == 0) {
+      listing.status = Status.COMPLETED;
     } else {
-      // Increment the flow rate for the next milestone
-      int96 newFlowRate = campaign.baseFlowRate *
-        int96(campaign.paymentCount + 1);
-      usdtx.updateFlow(campaign.creative, newFlowRate);
-      campaign.currentFlowRate = newFlowRate;
+      listing.status = Status.PAID;
     }
 
-    // Increment the payment count and check for completion
-    campaign.paymentCount++;
-
-    if (campaign.paymentCount == campaign.milestoneCount) {
-      // Complete the campaign and stop the stream
-      usdtx.deleteFlow(address(this), campaign.creative);
-      creatives[campaign.creative].xp += 20;
-
-      campaign.isCompleted = true;
-    }
+    emit ListingPaid(_id, _seller, msg.sender, _amount, _quantity);
   }
 
-  //for showing creative campaign payments
-  function getCreativeTotalPayment(
-    string calldata campaignId
-  ) external view returns (uint256) {
-    Campaign memory campaign = campaigns[campaignId];
-    return campaign.amount - ((systemFee * campaign.amount) / 100);
-  }
+  function deductCharge(uint256 _amount) internal pure returns (uint256) {
+    uint256 fee = _amount / 1000; // 0.1%
 
-  //get creative ratings
-  function getCreativeRating(
-    address _creative
-  ) external view returns (uint256) {
-    return creatives[_creative].xp;
-  }
-
-  function getSystemFee() external view returns (uint256) {
-    return systemFee;
-  }
-
-  function getCampaign(
-    string calldata campaignId
-  ) external view returns (Campaign memory) {
-    return campaigns[campaignId];
-  }
-
-  //to be deleted. Used for recovering testing funds
-  function withdraw(address _recipient) public payable {
-    payable(_recipient).transfer(address(this).balance);
+    return fee;
   }
 }
